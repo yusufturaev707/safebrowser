7dfa8a31e11665e52b08e52a7caddae4edeb81be
y.to'rayev <yusufturaev707@gmail.com>
2026-02-07 13:28:13 +0500

    window error 1

--- workers/monitor_checker.py
+++ workers/monitor_checker.py
@@ -84,13 +84,13 @@ class MonitorWorker(QThread):
     def get_cheating_monitor(self) -> bool:
         import platform
         os_name = platform.system()
-        print(f"OS name: {os_name}")
+        error(f"OS name: {os_name}")
 
         if os_name == "Windows":
             return self.get_windows_monitor_count() > 1
         elif os_name == "Linux":
             count_window = self.get_linux_monitor_count()
-            print(f"Linux monitor count: {count_window}")
+            error(f"Linux monitor count: {count_window}")
             return count_window > 1
         elif os_name == "Darwin":  # macOS
             #return self.get_macos_monitor_count()
